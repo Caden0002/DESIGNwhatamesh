afb6c7bc84edd20568644168cac877c90a6f971b
OmeletteX <chen.qixuan97@gmail.com>
2024-06-26 21:00:41 +0800

    “comment_out_basic_background”

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ export default function App() {
     return (
         <>
             <MeshGradientBackgroundLanding />
-            <MeshGradientBackground />
+            {/* <MeshGradientBackground /> */}
 
 
         </>
